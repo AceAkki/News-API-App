9c933fa7fc4078032299b2f34c513f1790e4d49a
Akshay P (A-K-SHA-Y) <140274081+AceAkki@users.noreply.github.com>
2023-10-02 23:50:43 +0530

    Add files via upload

---
+++ src/main/java/com/example/news/NewsService.java
@@ -0,0 +1,34 @@
+package com.example.news;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface NewsService {
+    @GET("top-headlines")
+    Call<NewsResponse> getTopHeadlines(
+            @Query("apiKey") String apiKey,
+            @Query("country") String country
+    );
+
+    @GET("top-headlines")
+    Call<NewsResponse> getTechNews(
+            @Query("apiKey") String apiKey,
+            @Query("category") String category
+    );
+
+    @GET("top-headlines")
+    Call<NewsResponse> getNewsByCountry(
+            @Query("apiKey") String apiKey,
+            @Query("country") String country
+    );
+
+
+    @GET("everything")
+    Call<NewsResponse> getNewsByKeyword(
+            @Query("apiKey") String apiKey,
+            @Query("q") String keyword,
+            @Query("sortBy") String sortBy
+    );
+
+}

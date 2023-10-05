3652d49dd8f62248329ce008298be1494f6f06a1
Akshay P (A-K-SHA-Y) <140274081+AceAkki@users.noreply.github.com>
2023-10-05 09:01:06 +0530

    Update CategoriesFragment.java
    Comments.

--- app/src/main/java/com/example/news/CategoriesFragment.java
+++ app/src/main/java/com/example/news/CategoriesFragment.java
@@ -26,10 +26,10 @@ public class CategoriesFragment extends Fragment {
     private Button searchButton;
     private NewsAdapter newsAdapter;
     private List<NewsArticle> newsArticles;
-    private static final String COUNTRY_CODE = "us"; // Set the country code to "us" for all categories
+    private static final String COUNTRY_CODE = "us";
 
     public CategoriesFragment() {
-        // Required empty public constructor
+        
     }
 
     @Override
@@ -49,16 +49,16 @@ public class CategoriesFragment extends Fragment {
         categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         categorySpinner.setAdapter(categoryAdapter);
 
-        // Initialize RecyclerView for displaying search results
+        // RecyclerView for displaying search results
         RecyclerView recyclerView = view.findViewById(R.id.categoriesRecyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         newsAdapter = new NewsAdapter(getContext());
         recyclerView.setAdapter(newsAdapter);
 
-        // Initialize the list to store search results
+    
         newsArticles = new ArrayList<>();
 
-        // Set up click listener for the search button
+    
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -70,19 +70,19 @@ public class CategoriesFragment extends Fragment {
     }
 
     private void performCategorySearch() {
-        String apiKey = "d3e5abf2bd454e63b6d226b88b492878"; // Replace with your News API key
+        String apiKey = "d3e5abf2bd454e63b6d226b88b492878"; 
         String selectedCategory = categorySpinner.getSelectedItem().toString().toLowerCase();
-        String countryCode = "us"; // Set the country code to "de" for Germany, you can change this as needed
+        String countryCode = "us"; 
 
-        // Make an API request to fetch news articles based on the selected category and country
+        
         NewsService newsService = RetrofitClient.getClient();
         newsService.getNewsByCategory(apiKey, countryCode, selectedCategory).enqueue(new Callback<NewsResponse>() {
             @Override
             public void onResponse(@NonNull Call<NewsResponse> call, @NonNull Response<NewsResponse> response) {
                 if (response.isSuccessful() && response.body() != null) {
-                    // Successfully fetched category results
+                    
                     newsArticles = response.body().getArticles();
-                    newsAdapter.setData(newsArticles); // Update the RecyclerView
+                    newsAdapter.setData(newsArticles); 
                 } else {
                     // Handle API error
                     Toast.makeText(getContext(), "Failed to fetch category results", Toast.LENGTH_SHORT).show();
